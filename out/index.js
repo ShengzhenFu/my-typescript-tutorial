7ae55f5af59f6e70bd1709763bf14f0d650abd42
ShengzhenFu <fushengzhen@163.com>
2021-04-04 17:17:32 +0800

    take input from npm start argument as github account

--- out/index.js
+++ out/index.js
@@ -1,11 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var GithubApiService_1 = require("./GithubApiService");
+var lodash_1 = require("lodash");
 var svc = new GithubApiService_1.GithubApiService();
-svc.getUserInfo("ShengzhenFu", function (user) {
-    console.log(user);
-    console.log("name: ", user.login);
-});
-svc.getRepoInfo("ShengzhenFu", function (repos) {
-    console.log(repos);
-});
+if (process.argv.length < 3) {
+    console.log('Please enter github account in parameter');
+}
+else {
+    svc.getUserInfo(process.argv[2], function (user) {
+        svc.getRepoInfo(process.argv[2], function (repos) {
+            var sortedRepos = lodash_1.sortBy(repos, [function (repo) { return repo.size * -1; }]);
+            user.repos = sortedRepos;
+            console.log(user);
+        });
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { GithubApiService } from './GithubApiService';
 import { Repo } from './Repo';
 import { User } from './User';
+import { sortBy } from 'lodash';
 
 let svc:GithubApiService = new GithubApiService();
-svc.getUserInfo("ShengzhenFu", (user: User)=>{
-    console.log(user);
-    console.log("name: ", user.login)
+// check if input of argument exists
+if (process.argv.length < 3) {
+    console.log('Please enter github account in argement');
+} else {    // take input from npm start as argument : process.argv[2]
+    svc.getUserInfo(process.argv[2], (user: User)=>{   
+        svc.getRepoInfo(process.argv[2], (repos: Repo[])=>{
+            let sortedRepos = sortBy(repos, [(repo:Repo) => repo.size * -1])
+            user.repos = sortedRepos;
+            console.log(user);
+    });
 });
-svc.getRepoInfo("ShengzhenFu", (repos: Repo[])=>{
-    console.log(repos);
-});
+}
+
